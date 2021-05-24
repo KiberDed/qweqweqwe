817b53181c6af489ab6bf916058bff5be10aef75
WillBeBetterQ <tuxto2000@mail.ru>
2021-05-24 10:21:17 +0300

    third

--- src/main/java/com/example/secTr/Rest/DeveloperRestController1.java
+++ src/main/java/com/example/secTr/Rest/DeveloperRestController1.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//Controller
+//Controllerasdasdasdasd
 @RestController
 @RequestMapping("/api/v1/developers")
 @PreAuthorize("hasAuthority('developers:write')")
